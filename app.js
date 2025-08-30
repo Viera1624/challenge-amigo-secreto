9ed85df9ce8f736383002443c6cc2ba2a8965b65
Victor_Viera <viera2845@gmail.com>
2025-08-29 20:08:19 -0500

    actualización proyecto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Variables
+let listaAmigos = [];
+
+function vaciarCampo() {
+    document.getElementById("amigo").value = '';
+}
+
+function agregarAmigo() {
+    //Guarda el nombre del amigo ingresado
+    let amigoIngresado = document.getElementById("amigo").value;
+
+    //Si el nombre tiene más de 10 caracteres
+    if (amigoIngresado.length > 10) {
+        alert("Ingrese un nombre valido, maximo 10 caracteres");
+
+        //Si el campo está vacío
+    } else {
+
+        if (amigoIngresado.length == 0) {
+            alert("Por favor ingrese un nombre");
+
+        } else {
+            //Mostrar la lista en la pagina
+            let listaMostrada = document.getElementById("listaAmigos"); //selecciona el elemento <ul> de html
+
+            if (listaAmigos.includes(amigoIngresado)) {
+                alert("El nombre ya está en la lista");
+                vaciarCampo();
+                return agregarAmigo();
+
+            }
+            let nuevoAmigo = document.createElement("li"); // Creamos un <li>
+            nuevoAmigo.textContent = amigoIngresado; // Asignamos el nombre del amigo
+            listaMostrada.appendChild(nuevoAmigo); // Lo añadimos a la lista que se va a mostrar
+
+
+            // Verifica si el amigo ya está en la lista
+            listaAmigos = Array.from(listaMostrada.children).map(li => li.textContent);
+            console.log(listaAmigos);
+            // si el amigo ya está en la lista
+
+        }
+
+        //limpiar el campo
+        vaciarCampo();
+
+
+
+
+
+    }
+
+
+}
