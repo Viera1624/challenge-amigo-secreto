692fb3a36f928f171d28300d3350bdacd415b20a
Victor_Viera <viera2845@gmail.com>
2025-09-04 16:10:07 -0500

    Actalización juego

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ function vaciarCampo() {
     document.getElementById("amigo").value = '';
 }
 
+function mostrarLista() {
+    let listaMostrada = document.getElementById("listaAmigos");
+    listaMostrada.innerHTML = "";
+
+    for (let posicion = 0; posicion < listaAmigos.length; posicion++) {
+        let amigo = document.createElement('li'); //Crea un elemento <li>
+        amigo.textContent = listaAmigos[posicion];
+        listaMostrada.appendChild(amigo);
+
+    }
+}
+
 function agregarAmigo() {
     //Guarda el nombre del amigo ingresado
     let amigoIngresado = document.getElementById("amigo").value;
@@ -12,34 +24,30 @@ function agregarAmigo() {
     //Si el nombre tiene más de 10 caracteres
     if (amigoIngresado.length > 10) {
         alert("Ingrese un nombre valido, maximo 10 caracteres");
+        vaciarCampo();
 
-        //Si el campo está vacío
     } else {
-
+        //Si el campo está vacío
         if (amigoIngresado.length == 0) {
             alert("Por favor ingrese un nombre");
 
         } else {
-            //Mostrar la lista en la pagina
-            let listaMostrada = document.getElementById("listaAmigos"); //selecciona el elemento <ul> de html
-
+            //Si el amigo ya está en la lista
             if (listaAmigos.includes(amigoIngresado)) {
                 alert("El nombre ya está en la lista");
                 vaciarCampo();
                 return agregarAmigo();
 
+            } else {
+                //Añadir amigo a la lista
+                listaAmigos.push(amigoIngresado);
+                console.log(listaAmigos);
             }
-            let nuevoAmigo = document.createElement("li"); // Creamos un <li>
-            nuevoAmigo.textContent = amigoIngresado; // Asignamos el nombre del amigo
-            listaMostrada.appendChild(nuevoAmigo); // Lo añadimos a la lista que se va a mostrar
-
-
-            // Verifica si el amigo ya está en la lista
-            listaAmigos = Array.from(listaMostrada.children).map(li => li.textContent);
-            console.log(listaAmigos);
-            // si el amigo ya está en la lista
 
         }
+
+        //Mostrar lista en la página
+        mostrarLista();
 
         //limpiar el campo
         vaciarCampo();
